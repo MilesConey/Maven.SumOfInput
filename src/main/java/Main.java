6f97ddb47af781f3038aa8e7576d139a89b76658
Miles Coney <MilesEConey@gmail.com>
2022-03-01 19:24:39 -0500

    Created a method to produce the sum of numbers 1 to n

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+//write a program which prompts the user to input a number n
+    // Program should respond by printing the sum of numbers 1 to n
+
 public class Main {
 
     public static void main(String[] args){
+        Scanner sumPrinter = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n = sumPrinter.nextInt();
+        int sum = 0;
 
+        for (int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
